05a70b9a579325d0551de86cb504d10a1590c70e
alexander <alexanderzoet@gmail.com>
2025-08-25 12:58:20 +0200

    navbar

---
+++ aiden-derksen-portfolio/src/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <main>
+        <NavBar />
+        <section class="introduction-container">
+            <h1>Aiden Derksen</h1>
+            <div class="text-container">
+                <p>Welcome to my portfolio! I'm Aiden Derksen, a passionate full-stack
+                    developer with a knack for creating dynamic and responsive web applications. Explore my projects and
+                    get in touch!</p>
+            </div>
+            <div class="introduction-image">
+                <img src="" alt="">
+            </div>
+        </section>
+    </main> 
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import '@/css/navbar.css';
+import '@/css/homepage.css';
+
+export default {
+    name: 'HomePage',
+    components: { NavBar }
+}
+</script>
---
+++ aiden-derksen-portfolio/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <h3>Aiden Derksen</h3>
+    <ul>
+      <li><a href="#about">About Me</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
