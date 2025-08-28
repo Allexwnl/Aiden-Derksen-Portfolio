8f3b20e0c4dd47912cc4280d4c17202be0543686
alexander <alexanderzoet@gmail.com>
2025-08-28 14:37:38 +0200

    added restricted acces to dashboard

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -5,12 +5,25 @@ import LoginAdmin from '../admin/LoginAdmin.vue';
 const routes = [
   { path: '/', component: HomePage },
   { path: '/admin', component: LoginAdmin },
-  { path: '/dashboard', component: () => import('../admin/DashBoard.vue') },
+  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.meta.requiresAuth
+  const user = supabase.auth.user()
+  const allowedEmails = ['alexanderzoet@gmail.com', 'aidenderksen2007@gmail.com'] // jouw admin emails
+
+  if (requiresAuth) {
+    if (!user || !allowedEmails.includes(user.email)) {
+      return next('/login') // redirect naar login als niet ingelogd of geen admin
+    }
+  }
+  next()
+})
 
 export default router;
