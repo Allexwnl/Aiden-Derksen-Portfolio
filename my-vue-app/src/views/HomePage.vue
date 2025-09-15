5e281821fcb1ba7b5fa9f5139b5c356ef257f5b8
alexander <alexanderzoet@gmail.com>
2025-09-15 14:56:18 +0200

    supabase keep alive

--- my-vue-app/src/views/HomePage.vue
+++ my-vue-app/src/views/HomePage.vue
@@ -1,52 +1,58 @@
 <template>
-    <main>
-        <NavBar />
-        <section class="introduction-container">
-            <div class="text-container">
-                <h1>Aiden Derksen</h1>
-                <p></p>
-            </div>
-            <div class="introduction-image">
-                <img src="../assets/img/aidenPF.jpg" alt="">
-            </div>
-        </section>
-        <section class="about-me-container">
-            <img src="../assets/img/camera.png" alt="">
-            <div class="about-me-text">
-                <h2 id="about">About Me</h2>
-                <p></p>
-            </div>
-        </section>
-        <section class="projects">
-            <div v-for="project in projects" :key="project.id" class="--projectCard">
-                <h2 class="text-xl">{{ project.title }}</h2>
-                <p>{{ project.description }}</p>
+  <main>
+    <NavBar />
+    <section class="introduction-container">
+      <div class="text-container">
+        <h1>Aiden Derksen</h1>
+        <p></p>
+      </div>
+      <div class="introduction-image">
+        <img src="../assets/img/aidenPF.jpg" alt="">
+      </div>
+    </section>
+    <section class="about-me-container">
+      <img src="../assets/img/camera.png" alt="">
+      <div class="about-me-text">
+        <h2 id="about">About Me</h2>
+        <p>Hallo! Mijn naam is Aiden Derksen en ben een jonge fotograaf. Mijn grootste interesse ligt in documentaire en
+          portret fotografie maar heb ook genoeg ervaring met product fotografie. Mensen fotograferen terwijl ze aan
+          iets werken is mijn passie. Het moment vastleggen terwijl ze gepassioneerd zijn en in focus, is zo gaaf om te
+          zien.
+          Ik ben een harde werker beide in een team of zelfstandig. Ik kan goed een studio opzetten en stijlen om een de
+          juiste sfeer over te brengen. Mijn foto’s zijn altijd in warme en kleurrijke tinten waardoor je je altijd
+          comfortabel bent en meevoelt met het beeld. Als je interesse hebt neem dan vooral contact met mij op!</p>
+      </div>
+    </section>
+    <section class="projects">
+      <div v-for="project in projects" :key="project.id" class="--projectCard">
+        <h2 class="text-xl">{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
 
-                <div v-if="project.images.length" class="relative w-64 h-64 mt-2">
-                    <img :src="project.images[project.currentImage]" class="project-images" />
+        <div v-if="project.images.length" class="relative w-64 h-64 mt-2">
+          <img :src="project.images[project.currentImage]" class="project-images" />
 
-                    <button v-if="project.images.length > 1" @click="prevImage(project)"
-                        class="absolute left-0 top-1/2 -translate-y-1/2 bg-black/50 text-white px-2 py-1 rounded">
-                        ‹
-                    </button>
+          <button v-if="project.images.length > 1" @click="prevImage(project)"
+            class="absolute left-0 top-1/2 -translate-y-1/2 bg-black/50 text-white px-2 py-1 rounded">
+            ‹
+          </button>
 
-                    <button v-if="project.images.length > 1" @click="nextImage(project)"
-                        class="absolute right-0 top-1/2 -translate-y-1/2 bg-black/50 text-white px-2 py-1 rounded">
-                        ›
-                    </button>
-                </div>
-            </div>
-        </section>
-        <div class="projects">
-    <div class="project">Project 1</div>
-    <div class="project">Project 2</div>
-    <div class="project">Project 3</div>
-    <div class="project">Project 4</div>
-    <div class="project">Project 5</div>
-    <div class="project">Project 6</div>
-</div>
+          <button v-if="project.images.length > 1" @click="nextImage(project)"
+            class="absolute right-0 top-1/2 -translate-y-1/2 bg-black/50 text-white px-2 py-1 rounded">
+            ›
+          </button>
+        </div>
+      </div>
+    </section>
+    <div class="projects">
+      <div class="project">Project 1</div>
+      <div class="project">Project 2</div>
+      <div class="project">Project 3</div>
+      <div class="project">Project 4</div>
+      <div class="project">Project 5</div>
+      <div class="project">Project 6</div>
+    </div>
 
-    </main>
+  </main>
 </template>
 
 <script setup>
@@ -84,7 +90,7 @@ const nextImage = (project) => {
 
 const prevImage = (project) => {
   if (!project.images.length) return
-  project.currentImage = 
+  project.currentImage =
     (project.currentImage - 1 + project.images.length) % project.images.length
 }
 
